b13b6a4d03890deaf7fef71c2807e69182aa4e4c
VgTan <alfonsusvega9@gmail.com>
2024-04-18 22:11:08 +0700

    feat:

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class LoggedIn
+class Admin
 {
     /**
      * Handle an incoming request.
@@ -15,11 +15,9 @@ class LoggedIn
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(Auth()->user() && Auth()->user()->usertype == 'user') {
-            return $next($request);
+        if(Auth()->user() && Auth()->user()->usertype == 'admin') {
+            return redirect('/dashboard');
         }
-        else {
-            return redirect("/login");
-        }
+        return $next($request);
     }
 }
